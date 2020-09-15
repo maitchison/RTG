cc411efee092d1ad4e10c24c571e47fd824ede90
Matthew Aitchison <matthewsaitchison@gmail.com>
2020-09-15 17:19:37 +1000

    very simple random actions envionment loop

--- rescue.py
+++ rescue.py
@@ -28,23 +28,10 @@ class MultiAgentEnv(gym.Env):
         super().__init__()
         pass
 
-    def step(self, action_n):
+    def step(self, actions):
         pass
 
     def reset(self):
-        pass
-
-
-    def _get_obs(self, agent):
-        """ get observation for a particular agent """
-        pass
-
-    def _get_reward(self, agent):
-        """ get reward for a particular agent """
-        pass
-
-    def _set_action(self, action, agent, action_space):
-        """ set env action for a particular agent """
         pass
 
     def render(self, mode='human'):
@@ -58,6 +45,7 @@ class RescueTheGeneralEnv(MultiAgentEnv):
     Rules:
     Players may occupy the same square.
     Red team and green team do not know the location of the general until they come within vision range
+    Rescuing and killing the general simultaneously counts as the general being killed.
 
     Action Space:
     (see below)
@@ -86,7 +74,6 @@ class RescueTheGeneralEnv(MultiAgentEnv):
     DX = [0, 0, -1, +1]
     DY = [-1, 1, 0, 0]
 
-
     def __init__(self):
         super().__init__()
 
@@ -94,34 +81,15 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         self.map_width = 64
         self.map_height = 64
         self.n_players = 12
-        self.map_layers = self.n_players + 1
+        self.map_layers = self.n_players + 2
         self.player_view_distance = 3
-
-        # general location
-        self.general_location = (np.random.randint(1, self.map_width - 1), np.random.randint(1, self.map_height - 1))
-
-        # create map
-        self.map = np.zeros((self.map_width, self.map_height, self.map_layers), dtype=np.uint8)
-        all_locations = list(itertools.product(range(self.map_width), range(self.map_height)))
-        for i in range(self.n_trees):
-            tree_locations = np.random.choice(all_locations, self.n_trees, replace=False)
-            for loc in tree_locations:
-                self.map[loc] = 1
-
-        # initialize players to random start locations, and assign initial health
-        general_filter = lambda x,y: x-self.general_location[0] > 3 and y-self.general_location[1] > 3
-
-        valid_start_locations = filter(general_filter, all_locations)
-        start_locations = np.random.choice(valid_start_locations, self.n_players, replace=False)
+        self.timeout = 1000
+        self.counter = 0
 
         self.player_location = np.zeros((self.n_players, 2), dtype=np.uint8)
         self.player_health = np.zeros((self.n_players), dtype=np.uint8)
         self.player_seen_general = np.zeros((self.n_players), dtype=np.uint8)
         self.player_team = np.zeros((self.n_players), dtype=np.uint8)
-
-        for i in range(self.n_players):
-            self.player_location[i] = start_locations[i]
-            self.player_health[i] = PLAYER_MAX_HEALTH
 
     def _in_vision(self, player_id, x, y):
         """
@@ -143,9 +111,9 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         assert 0 <= player_id < self.n_players, "Invalid player_id"
 
         x,y = self.player_location[player_id]
-        x = np.clip(x + dx, 0, self.map_width)
-        y = np.clip(y + dy, 0, self.map_height)
-        self.player_location[player_id] = x,y
+        x = np.clip(x + dx, 0, self.map_width-1)
+        y = np.clip(y + dy, 0, self.map_height-1)
+        self.player_location[player_id] = x, y
 
         # update general vision
         if self._in_vision(player_id, *self.general_location):
@@ -165,60 +133,71 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         rewards = np.zeros([self.n_players], dtype=np.float)
         dones = np.zeros([self.n_players], dtype=np.bool)
 
+        # dead men can't act
+        for i in range(self.n_players):
+            if self.player_health[i] <= 0:
+                actions[i] = self.ACTION_NOOP
+
         # apply actions, we process actions in the following order..
         # shooting
         # moving + acting
 
-        for player_id in range(self.n_players):
-            if player_id in range(self.n_players) in [self.ACTION_SHOOT_UP, self.ACTION_SHOOT_DOWN, self.ACTION_SHOOT_LEFT, self.ACTION_SHOOT_RIGHT]:
+        for i in range(self.n_players):
+            if i in range(self.n_players) in [self.ACTION_SHOOT_UP, self.ACTION_SHOOT_DOWN, self.ACTION_SHOOT_LEFT, self.ACTION_SHOOT_RIGHT]:
                 # shooting is not implemented yet...
                 pass
 
-        for player_id in range(self.n_players):
-            if actions[player_id] in [self.ACTION_MOVE_UP, self.ACTION_MOVE_DOWN, self.ACTION_MOVE_LEFT, self.ACTION_MOVE_RIGHT]:
-                indx = actions[player_id] - self.ACTION_MOVE_UP
-                self._move_player(player_id, self.DX[indx], self.DY[indx])
-            elif actions[player_id] == self.ACTION_ACT:
-                if self.general_location == self.player_location[player_id]:
+        for i in range(self.n_players):
+            if actions[i] in [self.ACTION_MOVE_UP, self.ACTION_MOVE_DOWN, self.ACTION_MOVE_LEFT, self.ACTION_MOVE_RIGHT]:
+                indx = actions[i] - self.ACTION_MOVE_UP
+                self._move_player(i, self.DX[indx], self.DY[indx])
+            elif actions[i] == self.ACTION_ACT:
+                px, py = self.player_location[i]
+                if self.general_location == (px, py):
                     rescue_counter += 1
-                if self.map[self.player_location[player_id]] == self.MAP_TREE:
+                if self.map[(px, py)] == self.MAP_TREE:
                     tree_counter += 1
-                    self.map[self.player_location[player_id]] = self.MAP_GRASS
+                    self.map[(px, py)] = self.MAP_GRASS
 
         # generate points
         general_killed = False
-        general_rescued = rescue_counter >= 2
+        general_rescued = rescue_counter >= 2 and not general_killed
+        game_timeout = self.counter >= self.timeout
 
-        for player_id in range(self.n_players):
-            if self.player_team == self.TEAM_GREEN:
-                rewards[player_id] += tree_counter
+        for i in range(self.n_players):
+            if self.player_team[i] == self.TEAM_GREEN:
+                rewards[i] += tree_counter
             if general_killed:
-                if self.player_team == self.TEAM_RED:
-                    rewards[player_id] += 10
-                elif self.player_team == self.TEAM_BLUE:
-                    rewards[player_id] -= 10
+                if self.player_team[i] == self.TEAM_RED:
+                    rewards[i] += 10
+                elif self.player_team[i] == self.TEAM_BLUE:
+                    rewards[i] -= 10
             elif general_rescued:
-                if self.player_team == self.TEAM_BLUE:
-                    rewards[player_id] += 10
-                elif self.player_team == self.TEAM_RED:
-                    rewards[player_id] -= 10
-
-        # end conditions
+                if self.player_team[i] == self.TEAM_BLUE:
+                    rewards[i] += 10
+                elif self.player_team[i]== self.TEAM_RED:
+                    rewards[i] -= 10
+            elif game_timeout:
+                if self.player_team[i] == self.TEAM_GREEN:
+                    rewards[i] += 10
 
         # send done notifications to players who are dead
-        for player_id in range(self.n_players):
-            dones[player_id] = self.player_health <= 0
+        for i in range(self.n_players):
+            dones[i] = self.player_health[i] <= 0
 
         # game ends for everyone under these outcome decisions
-        if general_killed or general_rescued:
+        if general_killed or general_rescued or game_timeout:
             dones[:] = True
 
-        obs = [self._get_player_observation(player_id) for player_id in range(self.n_players)]
+        obs = self._get_observations()
         infos = [{} for _ in range(self.n_players)]
+
+        self.counter += 1
 
         return obs, rewards, dones, infos
 
-
+    def _get_observations(self):
+        return [self._get_player_observation(player_id) for player_id in range(self.n_players)]
 
     def _get_player_observation(self, player_id):
         """
@@ -228,16 +207,13 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         :return: Numpy array containing player observation of dims [map_layers, width, height],
             first layer is map
             next layer is general position (if known)
-            then n_players layers with [0..1] at players location (if visible) indicating health
-
+            then n_players layers with number at players location (if visible) indicating health
         """
 
         assert 0 <= player_id < self.n_players, "Invalid player_id"
 
-        x, y = self.player_location[player_id]
-
         obs = np.zeros((self.map_layers, self.map_width, self.map_height), dtype=np.uint8)
-        obs[0, :, :] = self.map
+        obs[0, :, :] = self.map[:, :]
 
         if self.player_seen_general[player_id]:
             gx, gy = self.general_location
@@ -246,13 +222,54 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         for i in range(self.n_players):
             px, py = self.player_location[i]
             if self._in_vision(player_id, px, py):
-                obs[i+2, px, py] = self.player_health / PLAYER_MAX_HEALTH
+                obs[i+2, px, py] = self.player_health[i]
 
         return obs
 
-    # Vectorized wrapper for a batch of multi-agent environments
-# All environments must have the same observation and action space
-class VectorizedMultiAgentEnv(gym.Env):
-    def __init__(self):
+    def reset(self):
+        """
+        Reset game.
+        :return: observations
+        """
+
+        # general location
+        self.general_location = (np.random.randint(1, self.map_width - 2), np.random.randint(1, self.map_height - 2))
+
+        # create map
+        self.map = np.zeros((self.map_width, self.map_height), dtype=np.uint8)
+        all_locations = list(itertools.product(range(self.map_width), range(self.map_height)))
+        for i in range(self.n_trees):
+            idxs = np.random.choice(len(all_locations), self.n_trees, replace=False)
+            for loc in [all_locations[idx] for idx in idxs]:
+                self.map[loc] = 1
+
+        # initialize players to random start locations, and assign initial health
+        general_filter = lambda p: p[0] - self.general_location[0] > 3 and p[1] - self.general_location[1] > 3
+
+        valid_start_locations = list(filter(general_filter, all_locations))
+        start_locations = [valid_start_locations[idx] for idx in np.random.choice(len(valid_start_locations), self.n_players, replace=False)]
+
+        self.player_location *= 0
+        self.player_health *= 0
+        self.player_seen_general *= 0
+        self.player_team *= 0
+
+        # for the moment hardcode 4,4,4 teams
+        assert self.n_players == 12
+        teams = [self.TEAM_RED]*4 + [self.TEAM_GREEN]*4 + [self.TEAM_BLUE]*4
+        np.random.shuffle(teams)
+        self.player_team[:] = teams
+
+        for i in range(self.n_players):
+            self.player_location[i] = start_locations[i]
+            self.player_health[i] = PLAYER_MAX_HEALTH
+            self.player_seen_general[i] = self.player_team[i] == self.TEAM_BLUE
+            # this will update seen_general if player is close
+            # which normally doesn't happen as I make sure players do not start close to the general
+            self._move_player(i, 0, 0)
+
+        return self._get_observations()
+
+    def render(self, mode='human'):
         pass
 
--- train.py
+++ train.py
@@ -4,3 +4,26 @@ Train agents in the Rescue the General Game
 Author: Matthew Aitchison
 
 """
+
+from rescue import RescueTheGeneralEnv
+import numpy as np
+
+def main():
+
+    print("Starting environment")
+
+    env = RescueTheGeneralEnv()
+    obs = env.reset()
+
+    while True:
+        print(f"Turn {env.counter}")
+        # random actions for the moment...
+        actions = np.random.randint(0, 10, size=[env.n_players])
+        obs, rewards, dones, infos = env.step(actions)
+        if np.all(dones):
+            break
+
+
+
+if __name__ == "__main__":
+    main()
