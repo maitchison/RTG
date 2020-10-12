Fixes to masking

ppo_ma.py
@@ -171,18 +171,17 @@ class PPO_MA(ActorCriticRLModel):
                     self.train_mask_ph = tf.placeholder(tf.float32, [None], name="train_mask_ph")
 
                     neglogpac = train_model.proba_distribution.neglogp(self.action_ph)
-                    self.entropy = tf.reduce_mean(train_model.proba_distribution.entropy())
+                    self.entropy = mask_reduce(train_model.proba_distribution.entropy())
 
                     vpred = train_model.value_flat
 
+                    self.true_roles_ph = tf.placeholder(tf.int32, [n_batch_train, n_players], name="true_roles_ph")
 
                     if self.role_prediction:
                         # Role estimation loss
                         # We role the first two dims into one to make this work.
 
                         pred_role_logits = tf.reshape(train_model._role_fn, [n_batch_train * n_players, n_roles])
-                        self.true_roles_ph = tf.placeholder(tf.int32, [n_batch_train, n_players], name="true_roles_ph")
-
                         self.role_prediction_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
                             labels=tf.reshape(self.true_roles_ph, [n_batch_train * n_players]),
                             logits=pred_role_logits
@@ -193,7 +192,7 @@ class PPO_MA(ActorCriticRLModel):
                         self.role_prediction_loss = tf.reduce_mean(self.role_prediction_loss, axis=1)
                         self.role_prediction_loss = mask_reduce(self.role_prediction_loss)
                     else:
-                        self.role_prediction_loss = 0
+                        self.role_prediction_loss = tf.constant(0, dtype=tf.float32)
 
                     # Value function clipping: not present in the original PPO
                     if self.cliprange_vf is None:
@@ -310,7 +309,13 @@ class PPO_MA(ActorCriticRLModel):
         """
 
         advs = returns - values
-        advs = (advs - advs.mean()) / (advs.std() + 1e-8)
+
+        masked_advs = advs[train_mask == 1]
+        if len(masked_advs) == 0:
+            masked_advs = np.zeros([1]) # just in case we have all data masked out...
+
+        advs = (advs - masked_advs.mean()) / (masked_advs.std() + 1e-8)
+
         td_map = {self.train_model.obs_ph: obs, self.action_ph: actions,
                   self.advs_ph: advs, self.rewards_ph: returns,
                   self.learning_rate_ph: learning_rate, self.clip_range_ph: cliprange,
@@ -426,6 +431,7 @@ class PPO_MA(ActorCriticRLModel):
                             mb_env_inds = env_indices[start:end]
                             mb_flat_inds = flat_indices[mb_env_inds].ravel()
                             slices = (arr[mb_flat_inds] for arr in (obs, returns, masks, actions, values, neglogpacs, roles, train_mask))
+
                             mb_states = states[mb_env_inds]
                             mb_loss_vals.append(self._train_step(lr_now, cliprange_now, *slices,
                                                                  update=timestep, writer=writer, states=mb_states,
@@ -532,7 +538,7 @@ class Runner(AbstractEnvRunner):
             if self.model.role_prediction:
                 true_roles = self.env.get_roles()
             else:
-                true_roles = [0 for _ in range(self.env.num_envs)]
+                true_roles = np.zeros((self.env.num_envs, self.env.envs[0].n_players), dtype=np.int)
 
             mb_obs.append(self.obs.copy())
             mb_actions.append(actions)
@@ -548,8 +554,8 @@ class Runner(AbstractEnvRunner):
 
             train_mask = np.ones_like(actions, dtype=np.int)
             for i, info in enumerate(infos):
-                if "ignore_mask" in infos:
-                    train_mask[i] = infos["train_mask"]
+                if "train_mask" in info:
+                    train_mask[i] = info["train_mask"]
             mb_train_mask.append(train_mask)
 
             self.model.num_timesteps += self.n_envs

rescue.py
@@ -78,19 +78,19 @@ class RescueTheGeneralScenario():
             "team_counts": (2, 0, 4),
         },
 
-        "r2g4": {
-            "description": "Two red player and four green players on largish map",
-            "map_width": 48,
-            "map_height": 48,
-            "team_counts": (2, 4, 0),
-            "n_trees": 20,
-            "reward_per_tree": 0.5,
+        "r2g2": {
+            "description": "Two red players and two green players on a medium map",
+            "map_width": 32,
+            "map_height": 32,
+            "team_counts": (2, 2, 0),
+            "n_trees": 10,
+            "reward_per_tree": 1,
             "hidden_roles": "none",
             "max_view_distance": 5,             # makes thins a bit faster
             "team_view_distance": (5, 5, 5),    # no bonus vision for red
-            "team_shoot_range": (4, 1, 0),      # green can shoot, but not far
+            "team_shoot_range": (4, 4, 0),      # green can shoot, but not far
             "starting_locations": "random",     # random start locations
-            "team_shoot_timeout": (3, 5, 3)      # green is much slower at shooting
+            "team_shoot_timeout": (5, 5, 5)      # green is much slower at shooting
         },
 
         "red2": {
@@ -316,7 +316,15 @@ class RescueTheGeneralEnv(MultiAgentEnv):
     DX = [0, 0, -1, +1]
     DY = [-1, 1, 0, 0]
 
-    def __init__(self, scenario_name:str= "full", name:str= "env", log_file:str=None, **scenario_kwargs):
+    def __init__(self, scenario_name:str= "full", name:str= "env", log_file:str=None, dummy_prob=0, **scenario_kwargs):
+        """
+        :param scenario_name:
+        :param name:
+        :param log_file:
+        :param dummy_prob: Probability (0..1) that a player will be removed from game. This allows for a random number of
+            players in the game.
+        :param scenario_kwargs:
+        """
         super().__init__()
 
         self.env_create_time = time.time()
@@ -332,6 +340,7 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         self.name = name
         self.counter = 0
         self.game_counter = 0
+        self.dummy_prob = dummy_prob
 
         self.general_location = (0,0)
         self.general_health = int()
@@ -440,7 +449,8 @@ class RescueTheGeneralEnv(MultiAgentEnv):
         for action, player, info in zip(actions, self.players, infos):
 
             # if player was dead at start of round ignore this transition when training
-            info["train_mask"] = 0
+            if player.is_dead:
+                info["train_mask"] = 0
 
             player.action = ACTION_NOOP if player.is_dead else action
 
@@ -1104,6 +1114,15 @@ class RescueTheGeneralEnv(MultiAgentEnv):
             player.health = self.scenario.player_initial_health
             player.turns_until_we_can_shoot = player.shooting_timeout
             player.custom_data = dict()
+
+        # apply dummy players, we can do this by simply killing them
+        players_left_alive = len(self.players)
+        for player in self.players:
+            # make sure not to kill the last player
+            if np.random.rand() < self.dummy_prob and players_left_alive > 1:
+                player.health = 0
+                self.player_lookup[player.x, player.y] = -1
+                players_left_alive -= 1
 
         return np.asarray(self._get_observations())
 

train.py
@@ -100,7 +100,6 @@ class Config():
         self.cpus = 1 if tf.test.is_gpu_available else None  # for GPU machines its best to use only 1 CPU core
         self.parallel_envs = int()
         self.algo_params = dict()
-        self.algo = str()
         self.run = str()
         self.force_cpu = bool()
         self.script_blue_team = str()
@@ -328,32 +327,20 @@ def make_env(scenarios: Union[List[ScenarioSetting], ScenarioSetting, str], para
             else:
                 log_file = os.path.join(log_path, f"env_{index}.csv")
 
-            base_scenario = rescue.RescueTheGeneralScenario(scenario_setting.scenario_name)
-
-            make_env_fn = lambda team_counts, _strats=tuple(strats), _name=name, _scenario_setting=scenario_setting, _log_file=log_file: \
+            make_env_fn = lambda _strats=tuple(strats), _name=name, _scenario_setting=scenario_setting, _log_file=log_file: \
                 RTG_ScriptedEnv(
                     scenario_name=_scenario_setting.scenario_name, name=_name,
                     red_strategy=_strats[0],
                     green_strategy=_strats[1],
                     blue_strategy=_strats[2],
                     log_file=_log_file,
-                    **({'team_counts':team_counts} if team_counts is not None else {})
+                    dummy_prob=0.25 if vary_players else 0
                 )
 
-            # if we have 'random' team players enabled we need to add a selection of environments with different
-            # player counts
-            if vary_players:
-                for r in reversed(range(base_scenario.team_counts[0]+1)):
-                    for g in reversed(range(base_scenario.team_counts[1]+1)):
-                        for b in reversed(range(base_scenario.team_counts[2]+1)):
-                            if r == g == b == 0:
-                                continue
-                            env_functions.append(lambda _r=r, _g=g, _b=b: make_env_fn((_r, _g, _b)))
-            else:
-                env_functions.append(lambda: make_env_fn(None))
+            env_functions.append(make_env_fn)
 
     vec_env = MultiAgentVecEnv(env_functions)
-    print(f"Created vector environment with {parallel_envs} parallel copies and {vec_env.num_envs} total agents.")
+    #print(f"Created vector environment with {parallel_envs} parallel copies and {vec_env.num_envs} total agents.")
     return vec_env
 
 def train_model():
@@ -380,8 +367,10 @@ def train_model():
     scenario_descriptions = set(str(env.scenario) for env in vec_env.envs)
     for description in scenario_descriptions:
         print(description)
+    print()
     print("Config:")
     print(config)
+    print()
 
     model = make_model(vec_env)
 
@@ -483,6 +472,10 @@ def make_model(vec_env: MultiAgentVecEnv, model_name = None, verbose=0):
     #     f"Invalid mini_batch size {config.algo_params['mini_batch_size']}, must divide batch size {batch_size}."
     # n_mini_batches = batch_size // config.algo_params["mini_batch_size"]
     # print(f" -using {n_mini_batches} mini-batch(s) of size {config.algo_params['mini_batch_size']}.")
+
+    batch_size = config.algo_params["n_steps"] * vec_env.num_envs
+    mini_batch_size = batch_size // config.algo_params["nminibatches"]
+    print(f"Model created, using batch size of {batch_size} and mini-batch size of {mini_batch_size}")
 
     params = config.algo_params.copy()
 