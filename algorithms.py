fixes to kl logging

algorithms.py
@@ -173,6 +173,7 @@ class PMAlgorithm(MarlAlgorithm):
         self.dm_loss_scale = dm_loss_scale
         self.dm_kl_factor = dm_kl_factor
         self.deception_batch_counter = 0
+        self.policy_batch_counter = 0
 
         # the type of prediction to use for deception off|action|observation|both
         self.prediction_mode = prediction_mode
@@ -1126,10 +1127,10 @@ class PMAlgorithm(MarlAlgorithm):
         # note: this is a little odd as the input will be role dependant, but it should still give us some idea
         # of how the policies diverge
         # also, this is a little slow so we do it only on occasion
-        if self.deception_batch_counter % 10 == 9:
+        if self.policy_batch_counter % 10 == 9:
             for role_a in range(3):
                 a = data["role_log_policy"][..., role_a, :]
-                entropy = torch.sum(- torch.exp(a) * a, dim=-1)
+                entropy = torch.sum(-torch.exp(a) * a, dim=-1).mean()
                 self.log.watch_mean(f"entropy_{role_a}", entropy, display_width=0)
                 for role_b in range(3):
                     b = data["role_log_policy"][..., role_b, :]
@@ -1185,6 +1186,8 @@ class PMAlgorithm(MarlAlgorithm):
             self.log.watch_mean("s_scale", self.scaler.get_scale())
         else:
             (-loss).backward()
+
+        self.policy_batch_counter += 1
 
     @profiler.record_function("deception_back")
     def forward_deception_mini_batch(self, data, loss_scale=1):
@@ -1620,6 +1623,7 @@ class PMAlgorithm(MarlAlgorithm):
         batch_data["ext_value"] = self.batch_ext_value
         batch_data["int_value"] = self.batch_int_value
         batch_data["log_policy"] = self.batch_log_policy
+        batch_data["role_log_policy"] = self.batch_role_log_policies # only needed for debugging
         batch_data["advantages"] = self.batch_advantage
         batch_data["terminals"] = self.batch_terminals
         batch_data["rnn_states"] = self.extract_policy_rnn_states(self.batch_rnn_states)

video.py
@@ -121,6 +121,9 @@ def export_video(filename, algorithm: PMAlgorithm, scenario):
     last_outcome = ""
     bonus = None
 
+    def channels_first_to_last(x):
+        return x.swapaxes(0, 1).swapaxes(1, 2)
+
     # play the game...
     while last_outcome == "":
 
@@ -162,7 +165,7 @@ def export_video(filename, algorithm: PMAlgorithm, scenario):
             for i in range(n_players):
                 dx = 0
                 dy = orig_height + i * obs_size
-                frame[dy:dy + obs_size, dx:dx + obs_size] = obs_truth[i].swapaxes(0, 1)
+                frame[dy:dy + obs_size, dx:dx + obs_size] = channels_first_to_last(obs_truth[i])
 
             # predictions
             # observation frames are [n_players, n_predictions, h, w, c]
@@ -172,9 +175,8 @@ def export_video(filename, algorithm: PMAlgorithm, scenario):
                 for j in range(n_predictions):
                     dx = j * obs_size + obs_size
                     dy = orig_height + i * obs_size
-                    # we transpose as rescue is x,y instead of y,x
-                    frame[dy:dy + obs_size, dx:dx + obs_size] = \
-                        np.asarray(obs_predictions[i, j] * 255, dtype=np.uint8).swapaxes(0, 1)
+                    predictions_transposed = channels_first_to_last(np.asarray(obs_predictions[i, j] * 255, dtype=np.uint8))
+                    frame[dy:dy + obs_size, dx:dx + obs_size] = predictions_transposed
 
         if "obs_backwards_prediction" in model_output:
             obs_pp = model_output["obs_backwards_prediction"].detach().cpu().numpy()
@@ -183,8 +185,8 @@ def export_video(filename, algorithm: PMAlgorithm, scenario):
                     dx = j * obs_size + (obs_size * (n_players + 1))
                     dy = orig_height + i * obs_size
                     # we transpose as rescue is x,y instead of y,x
-                    frame[dy:dy + obs_size, dx:dx + obs_size] = \
-                        np.asarray(obs_pp[j, i] * 255, dtype=np.uint8).swapaxes(0, 1)
+                    predictions_transposed = channels_first_to_last(np.asarray(obs_pp[j, i] * 255, dtype=np.uint8))
+                    frame[dy:dy + obs_size, dx:dx + obs_size] = predictions_transposed
 
         if "action_prediction" in model_output:
             action_predictions = np.exp(model_output["action_prediction"].detach().cpu().numpy())